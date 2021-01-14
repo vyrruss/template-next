19b4744809ef2d50acd37d6939379c6ab71cc6f0
Gaetan Boedec <gboedec@me.com>
2021-01-14 15:43:49 +0100

    Add basic config for TS and next

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // target: 'serverless', // Enable For deploy with Netlify
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+  },
+};
---
+++ pages/_app.tsx
@@ -0,0 +1,17 @@
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+
+import '../styles/globals.css';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
