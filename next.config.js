60ec1f59db702ac2150d2a2e774c4d3f013a9448
Gaetan Boedec <gboedec@me.com>
2021-04-13 18:02:26 +0200

    Upgrade dependencies to latest

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   // target: 'serverless', // Enable For deploy with Netlify
+  future: {
+    webpack5: true,
+  },
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'en',
